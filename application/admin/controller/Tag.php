041db352f41ac32d02000cfbe147a71f14c635f1
zhaowanjiang <876691006@qq.com>
2017-11-30 17:46:04 +0800

    创建项目

---
+++ application/admin/controller/Tag.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/30
+ * Time: 16:47
+ */
+
+namespace app\admin\controller;
+
+
+class Tag extends Base
+{
+    //标签云
+    public function index(){
+
+        return view();
+    }
+}
