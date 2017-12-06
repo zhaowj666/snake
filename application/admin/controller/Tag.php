fca05d4ba164e145895f187d6c10d637b99b90d8
zhaowanjiang <876691006@qq.com>
2017-12-06 14:30:38 +0800

    标签查询

--- application/admin/controller/Tag.php
+++ application/admin/controller/Tag.php
@@ -10,6 +10,7 @@ namespace app\admin\controller;
 
 
 use app\admin\model\TagModel;
+use think\Request;
 
 class Tag extends Base
 {
@@ -22,5 +23,42 @@ class Tag extends Base
         $list = $tag->getList($map);
         $this->assign('list',$list);
         return view();
+    }
+
+    /**
+     * 添加编辑标签
+     */
+    public function tagAdd(){
+        $tag = new TagModel();
+        if(Request::instance()->isPost()){  //post请求
+            $id = input('post.id');
+            $data['title']          = input('param.title');
+            if(empty($id)){
+                $data['status']         = 1;
+                $data['create_time']    = time();
+            }else{
+                $data['id'] = $id;
+            }
+            $rst = $tag->tagAdd($data);
+            return json(msg($rst['code'],$rst['data'],$rst['msg']));
+
+        }elseif(Request::instance()->isGet()){  //get请求
+            $id = input('get.id');
+            if($id){
+                $info = $tag->getOne($id);
+                $this->assign($info);
+            }
+            return view();
+        }
+    }
+
+    /**
+     * 删除标签
+    */
+    public function tagDel(){
+        $id = input('get.id');
+        $tag = new TagModel();
+        $rst = $tag->tagDel($id);
+        return json(msg($rst['code'],$rst['data'],$rst['msg']));
     }
 }
--- application/admin/model/TagModel.php
+++ application/admin/model/TagModel.php
@@ -12,4 +12,34 @@ class TagModel extends Model{
         $list = $this->where($map)->limit(20)->select();
         return $list;
     }
+    //获取一条数据
+    public function getOne($id){
+        return $this->find($id);
+    }
+
+    //添加编辑
+    public function tagAdd($data){
+        if(empty($data['id'])){
+            if($this->insert($data) !== false){
+                return ['code'=>true,'data'=>'','msg'=>'添加成功'];
+            }else{
+                return ['code'=>false,'data'=>'','msg'=>'添加失败'];
+            }
+        }else{
+            if($this->update($data) !== false){
+                return ['code'=>true,'data'=>'','msg'=>'编辑成功'];
+            }else{
+                return ['code'=>false,'data'=>'','msg'=>'编辑失败'];
+            }
+        }
+    }
+
+    //删除标签
+    public function tagDel($id){
+        if($this->where(['id'=>$id])->delete() === false){
+            return ['code'=>false,'data'=>'','msg'=>'删除失败'];
+        }else{
+            return ['code'=>true,'data'=>'','msg'=>'删除成功'];
+        }
+    }
 }
