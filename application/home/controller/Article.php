81f924463f526f5a83ba805c462858219983350c
zwj <876691006@qq.com>
2017-12-28 22:53:00 +0800

    article list

--- application/home/controller/Article.php
+++ application/home/controller/Article.php
@@ -7,13 +7,34 @@ use app\home\Model\TagModel;
 class Article extends Base
 {
 
+    public function index(){
+        $param = input('param.');
+        $where = [
+            'status'    => 1,
+            'type'    => 1,
+        ];
+        //查询关键字
+        if (!empty($param['searchText'])) {
+            $where['title'] = ['like', '%' . $param['searchText'] . '%'];
+        }
+        //标签文章
+        if (!empty($param['tag'])) {
+
+        }
+        $article = new ArticleModel();
+        $selectResult = $article->getList($where,0,10);
+        $this->assign('meta_title', '文章列表');
+        $this->assign('list', $selectResult);
+        return $this->fetch();
+    }
+
     public function detail($id)
     {
         $article = new ArticleModel();
         $info = $article->getOne($id);
         //热门文章
         $where['status']    = 1;
-        $hotArticles = $article->getList($where,0,6,'id,title,keywords');
+        $hotArticles = $article->getList($where,0,10,'id,title,keywords');
         //标签
         $tag = new TagModel();
         $tagList = $tag->getList();
--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -10,11 +10,12 @@ class Index extends Base
     public function index()
     {
         $article = new ArticleModel();
-        //主页文章
-        $where['status']    = 1;
-        $articleList = $article->getList($where,0,8,'id,title,thumbnail,keywords');
         //热门文章
-        $hotArticles = $article->getList($where,0,6,'id,title,keywords');
+        $where['status']    = 1;
+        $hotArticles = $article->getList($where,0,6,'id,title,keywords','browse desc,id desc');
+        //主页文章
+        $where['type']    = 1;
+        $articleList = $article->getList($where,0,8,'id,title,thumbnail,keywords');
         //标签
         $tag = new TagModel();
         $tagList = $tag->getList();
